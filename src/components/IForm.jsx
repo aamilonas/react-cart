d89a9b420d36efcfe0d24babbbc1b1fa99fc6bfd
aamilonas <angelomilonas1@gmail.com>
2023-11-28 22:39:54 -0500

    sadfasf

--- src/components/IForm.jsx
+++ src/components/IForm.jsx
@@ -7,14 +7,26 @@ import Image from "react-bootstrap/Image";
 import sbag from "../images/sbag.png";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
+import DisplayList from "./DisplayList";
 
 const IForm = (props) => {
   const initialForm = {
     iname: "",
     quantity: 1,
-    price: 0,
+    price: 0.0,
     subtotal: 0.0,
   };
+
+  const ErrorStyle = {
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    color: "#eb6f92",
+  };
+
+  const [errors, setErrors] = useState([]);
+
+  const [itemList, setItemList] = useState([]);
 
   const [item, setItem] = useState(initialForm);
 
@@ -26,11 +38,38 @@ const IForm = (props) => {
     if (!isNaN(item.quantity) && !isNaN(item.price)) {
       item.subtotal = item.quantity * item.price;
     }
-    console.log(item.quantity);
-    console.log(item.price);
-    console.log(item.subtotal);
+    document.getElementById("st").value = item.subtotal.toFixed(2);
   };
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
 
+    const errorList = [];
+
+    if (item.iname === "") {
+      errorList.push("⚠️ Item must have a valid name! ⚠️");
+    }
+    if (item.quantity <= 0) {
+      errorList.push(
+        "⚠️ Quantity of items must be an integer greater than zero! ⚠️"
+      );
+    }
+    if (item.price <= 0) {
+      errorList.push("⚠️ Item price must be a valid number! ⚠️");
+    }
+
+    if (Number.isInteger(Number(item.price)) && Number(item.price) > 0) {
+      let num = Number(item.price);
+      item.price = num.toFixed(2);
+    }
+
+    if (errorList.length > 0) {
+      setErrors(errorList);
+    } else {
+      setItemList([...itemList, item]);
+      setItem(initialForm);
+      setErrors([]);
+    }
+  };
   return (
     <>
       <Form.Label htmlFor="item" className=" fs-2">
@@ -61,7 +100,7 @@ const IForm = (props) => {
         <Row xs={2} md={4} lg={6}>
           <Col xs lg="3">
             <input
-              name="itemName"
+              name="iname"
               type="text"
               className="form-control"
               id="exampleInputPassword1"
@@ -72,6 +111,7 @@ const IForm = (props) => {
           <Col md={{ span: 6, offset: 0 }}>
             <Form.Control
               type="number"
+              name="quantity"
               className="form-control"
               value={item.quantity}
               onChange={onChangeHandler}
@@ -83,8 +123,10 @@ const IForm = (props) => {
               <InputGroup.Text>$</InputGroup.Text>
               <Form.Control
                 aria-label="Dollar amount (with dot and two decimal places)"
+                name="price"
                 value={item.price}
                 type="number"
+                placeholder="0.00"
                 className="form-control"
                 onChange={onChangeHandler}
                 onBlur={onBlurHandler}
@@ -94,18 +136,35 @@ const IForm = (props) => {
           <Col>
             <Form.Control
               type="number"
+              id="st"
+              name="subtotal"
               placeholder="0.00"
               aria-label="Disabled input example"
-              value={item.subtotal}
+              value={item.subtotal.toFixed(2)}
               disabled
               readOnly
             />
           </Col>
           <Col>
-            <Button variant="outline-success">Add to Shopping List</Button>{" "}
+            <Button variant="outline-success" onClick={onSubmitHandler}>
+              Add to Shopping List
+            </Button>{" "}
           </Col>
         </Row>
+        <Row className="justify-content-center">
+          {errors.map((err, i) => (
+            <p style={ErrorStyle} key={i}>
+              {err}
+            </p>
+          ))}
+        </Row>
       </Container>
+      <DisplayList list={itemList} />
+      {/* {itemList.length > 0 ? (
+        Add the total stuff here
+      ) : (
+        <p> </p>
+      )} */}
     </>
   );
 };
